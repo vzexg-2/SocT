2ac01e425189636530804ab409ef44fa22e9f5de
SunshineJ <155290329+vzexg-2@users.noreply.github.com>
2024-05-12 21:36:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ apt-get upgrade
 apt install nodejs -y && apt install node -y
 apt install npm -y
 npm install cloudscraper -y && npm install http -y && npm install https -y && npm install requests -y
-clear && echo "[+] Installed successfully"
+ulimit -n 99999
+clear && echo "[!] Failed? type (sudo su)"
