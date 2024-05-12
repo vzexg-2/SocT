5d2a937814ec9e2ecbea9f4a89cbca4e1f4df239
SunshineJ <155290329+vzexg-2@users.noreply.github.com>
2024-05-12 21:34:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get upgrade
+apt install nodejs -y && apt install node -y
+apt install npm -y
+npm install cloudscraper -y && npm install http -y && npm install https -y && npm install requests -y
+clear && echo "[+] Installed successfully"
