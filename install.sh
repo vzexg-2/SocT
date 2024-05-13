d5f2a15fbf2526f20ca86db5febebb1889c51fa2
SunshineJ <155290329+vzexg-2@users.noreply.github.com>
2024-05-13 12:19:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ apt-get update
 apt-get upgrade
 apt install nodejs -y && apt install node -y
 apt install npm -y
-npm install cloudscraper -y && npm install http -y && npm install https -y && npm install net -y
+npm install cloudscraper -y && npm install http -y && npm install https -y && npm install net -y && npm install puppeteer -y
 ulimit -n 99999
-clear && echo "[!] Failed? type (sudo su)"
+clear && echo "[!] IF failed, type (sudo su)"
