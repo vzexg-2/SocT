1c00dae0c8cce88d490697c3fc9596ee79af5ed9
SunshineJ <155290329+vzexg-2@users.noreply.github.com>
2024-05-14 12:05:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ apt install npm -y
 npm install cloudscraper -y && npm install http -y && npm install https -y && npm install net -y && npm install puppeteer -y
 ulimit -n 99999
 clear && echo "[!] IF failed, type (sudo su)"
+clear; node SocT.js
